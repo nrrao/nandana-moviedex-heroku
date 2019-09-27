acb1950111d53e7590946f543106f82de3247281
nrrao <nandanarao@yahoo.com>
2019-09-27 14:40:26 -0400

    done steps for deploy

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const MOVIES = require('./movies-data-small.json')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const MOVIES = require('./movies-data-small.json');
 
-const app = express()
+const app = express();
 
-app.use(morgan('dev'))
-app.use(cors())
-app.use(helmet())
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting))
+app.use(cors());
+app.use(helmet());
 
 app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    return res.status(401).json({ error: 'Unauthorized request' })
+    return res.status(401).json({ error: 'Unauthorized request' });
   }
-  next()
-})
+  next();
+});
 
 app.get('/movie', function handleGetMovie(req, res) {
   let response = MOVIES;
@@ -26,26 +27,35 @@ app.get('/movie', function handleGetMovie(req, res) {
   if (req.query.genre) {
     response = response.filter(movie =>
       movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
-    )
+    );
   }
 
   if (req.query.country) {
     response = response.filter(movie =>
       movie.country.toLowerCase().includes(req.query.country.toLowerCase())
-    )
+    );
   }
 
   if (req.query.avg_vote) {
     response = response.filter(movie =>
       Number(movie.avg_vote) >= Number(req.query.avg_vote)
-    )
+    );
   }
 
-  res.json(response)
-})
+  res.json(response);
+});
 
-const PORT = 9000
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
